73dfaf6d09f50f0b3e95354041b328b95161d5f5
samwijayag <sam.wijayag@gmail.com>
2019-01-29 11:17:13 -0800

    database connected

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password: 'test',
+    database : 'smart-brain'
+  }
+});
+
 
 const app = express();
 
@@ -40,24 +52,21 @@ app.post('/signin', (req,res) => {
 	} else {
 		res.status(400).json('Error logging in');
 	}
-	console.log(req.body.email);
-	console.log(req.body.password);
 })
 
 app.post('/register', (req,res)=> {
 	const {email,name,password} = req.body;
-	bcrypt.hash(password, null, null, function(err, hash) {
-    	console.log(hash);
-	});
-	database.users.push({
-		id: '132',
+	db('users') 
+	.returning('*')
+	.insert({
 		email: email,
 		name: name,
-		password: password,
-		entries: 0,
 		joined: new Date()
 	})
-	res.json(database.users[database.users.length-1])
+	.then(user => {
+		res.json(user[0])	
+	})
+	.catch(err => res.status(400).json('unable to register'))
 })
 
 
@@ -89,14 +98,7 @@ app.put('/image',(req,res)=>{
 		res.status(400).json('user invalid')
 	}
 })
- 
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
+
 
 
 app.listen(3000, () => {
